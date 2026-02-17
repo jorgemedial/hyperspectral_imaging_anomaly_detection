a27dba058f2b7e86efe1d402792f8391b64fde2c
jorge <jorgemedial@gmail.com>
2026-02-17 20:58:25 +0100

    fix heatmap

--- simplenet_test.py
+++ simplenet_test.py
@@ -28,8 +28,8 @@ def export_as_image(anomaly_map: torch.Tensor):
     arr = anomaly_map.squeeze(-1).cpu().numpy()     # → (H, W)
 
     # Normalize to 0–255 if values are not already in that range
-    arr = F.relu(arr - 0.5) # values below 0.5 were not penalized.
-    arr = 255 - F.relu(255 - arr * 1000).astype(np.uint8) # Equivalent to min(255, arr * 1000). The arr values are scaled up for visualization
+    arr = max(0, arr - 0.5) # values below 0.5 were not penalized.
+    arr = min(255, arr * 1000).astype(np.uint8) # The arr values are scaled up for visualization
     
     return Image.fromarray(arr, mode="L")         # "L" = 8-bit grayscale
 
